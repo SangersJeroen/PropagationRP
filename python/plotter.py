c90ab1526c57e6746bdcef70abd9c24cce164ce9
Jeroen Sangers <jeroen98.sangers@gmail.com>
2020-04-05 21:56:08 +0200

    Eerste deel mirage af

--- python/plotter.py
+++ python/plotter.py
@@ -14,38 +14,23 @@ mpb.rcParams.update({
 
 #Defining a few constants
 n0 = 1.0002512365013845
-alpha = 0.01
+alpha = -7.77e-5
 theta = 30
-sin = np.sin(np.pi/180*theta)
-c = 0.5*(-np.sqrt(16*alpha**2 +16*alpha*n0+sin**2+4*n0**2)-sin)
-k = np.log((np.sqrt(16*alpha**2 +16*alpha*n0+sin**2+4*n0**2)+4*alpha+sin+2*n0)/(np.sqrt(16*alpha**2 +16*alpha*n0+sin**2+4*n0**2) -4*alpha +sin-2*n0))/2*alpha
+c = np.sin(np.pi/180*theta)
+x = np.linspace(0,1000,10000)
+k = 0
 
-x = np.linspace(0,100,10000)
-print(c)
+def z(x,k,c):
+    return c**2 *(-np.exp(alpha*x/c + alpha*k) -np.exp(-alpha*x/c -alpha*k) + 2*n0)/(2*alpha)
 
-""" def z(x,c):
-    upper = -n0*np.exp(2*alpha*(x-c)) + c*(-np.exp(2*alpha*(x-c))) + c - n0
-    lower = alpha*(np.exp(2*alpha*(x-c))+1)
-    return upper/lower
- """
-"""
-def dzdx(x,z):
-    zprime = np.sqrt((n0+alpha*z)**2 -c**2)/c
-    return zprime
- """"""
+zx = z(x,k,c)
+dzdx = np.asarray([])
+for i in range(1,len(zx)-1):
+    dzdx = np.append(dzdx, (zx[i+1]-zx[i])/(x[i+1]-x[i]))
+    angle = np.arcsin(dzdx)*180/np.pi
 
-def y(x):
-    return (np.exp(-(alpha**2 * x)/c)*(-2*b*np.exp((alpha**2 * x)/c) -4*alpha**2 * const + np.exp(2*alpha*x/c) +b**2 ))/(4*alpha**2)
-"""
-
-def z(c,x,alpha):
-    return -n0+1.7 - (c*np.cosh(alpha*(x+k)))
-
-for i in [alpha,alpha*0.9,alpha*0.8,alpha*0.7,alpha*0.6,alpha*0.5,alpha*0.4,alpha*0.3]:
-    plt.plot(x,z(c,x,i), label='alpha = {:.3f}'.format(i))
-    plt.xlabel("Distance $m$")
-    plt.ylabel("Height $m$")
-#plt.plot(x,z1(c,x), label='Light z1')
-#plt.ylim([0,z1(c,x)[-1]])
+print(zx[0],zx[-1])
+plt.plot(x,zx, label="optical path")
+plt.plot(x[1:-1],angle, label="path angle with horizontal")
 plt.legend()
 plt.show()
